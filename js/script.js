d10aee67a15416f0b084c5facc3ec4fa410182a2
Variic <variclothar@gmail.com>
2021-09-07 11:18:06 +0100

    ADD GENERATING LINKS TAGS IN ARTICLES

--- js/script.js
+++ js/script.js
@@ -45,62 +45,49 @@
   };
 
 
-  /*const optArticleSelector = '.post',
-    optTitleSelector = '.post-title',
-    optTitleListSelector = '.titles',
-    optArticleTagsSelector = '.post-tags .list';*/
+  /*--------------------------------------------------------------------------*/
 
 
   const generateTitleLinks = function(){/*ROZNICA*/
-    console.log('Title was clicked');
 
 
     /* remove contents of titleList */
     const titleList = document.querySelector('ul.list');/*ROZNICA*/
-    console.log(titleList);
     titleList.innerHTML= '';
 
 
     /* find all the articles and save them to variable: articles */
     const articles = document.querySelectorAll('.post');/*ROZNICA*/
-    console.log('Show posts list!', articles);
 
     let html = '';
 
     for(let article of articles){
-      console.log('Show let HTML!', html);
 
 
       /* get the article id */
       const articleId = article.getAttribute('ID');
-      console.log('Show articles ID', articleId);
 
 
       /* find the title element */
       const titleList = document.querySelector('.titles');
-      console.log('Show title element!', titleList);
 
 
       /* get the title from the title element */
       const articleTitle = article.querySelector('h3.post-title').innerHTML;/*ROZNICA*/
-      console.log('Show articles title', articleTitle);
 
 
       /* create HTML of the link */
       const linkHtml = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-      console.log('Show generated HTML link!', linkHtml);
 
 
       /* insert link into html variable */
       html = html + linkHtml;
-      console.log('Show full HTML!', html);
     }
 
     titleList.innerHTML = html;
 
 
     const links = document.querySelectorAll('.titles a');
-    console.log('What we have on LINKS', links);
 
     for(let link of links){
       link.addEventListener('click', titleClickHandler);
@@ -111,30 +98,54 @@
   generateTitleLinks();
 
 
+  /*--------------------------------------------------------------------------*/
+
+
+  const optArticleTagsSelector = '.post-tags .list';
+
   function generateTags(){
+
     /* find all articles */
+    const articles = document.querySelectorAll('.post');/*ROZNICA*/
+    console.log('SHOW POST LIST!', articles);
 
     /* START LOOP: for every article: */
+    for(let article of articles){
 
     /* --- find tags wrapper */
+    const wrapperList = article.querySelector(optArticleTagsSelector);
 
     /* --- make html variable with empty string */
+    let html = '';
 
     /* --- get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
+    console.log('SHOW ARTICLE TAGS!', articleTags);
 
     /* --- split tags into array */
+    const articleTagsArray = articleTags.split(' ');
+    console.log('SHOW TAGS ARRAY!', articleTagsArray);
 
     /* --- START LOOP: for each tag */
+    for(let tag of articleTagsArray){
+      console.log('SHOW TAG!', tag);
 
     /* --- --- generate HTML of the link */
+    const linkHtml = '<li><a href="#tag-' + tag + '">' + tag + '</a></li>';
+    console.log('SHOW LINK HTML!', linkHtml);
 
     /* --- --- add generated code to html variable */
-
+    html = html + linkHtml;
+    console.log('SHOW LET HTML!', html);
+    }
     /* --- END LOOP: for each tag */
 
     /* --- insert HTML of all the links into the tags wrapper */
+    wrapperList.innerHTML = html;
+    console.log('SHOW WRAPPER LIST!', wrapperList);
 
     /* END LOOP: for every article: */
+  }
   }
 
   generateTags();
